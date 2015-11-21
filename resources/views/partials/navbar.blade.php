042a24c03596fa5c80172507c0228329274ba8ff
Nick Tolkachev <nick8markelov@gmail.com>
2015-11-21 14:52:57 +0300

    Update navbar.blade.php

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -6,4 +6,28 @@
         <li class="visible-md visible-lg"><a href="#" id="main-menu-toggle"><i class="fa fa-bars"></i></a></li>
         <li class="visible-xs visible-sm"><a href="#" id="sidebar-menu"><i class="fa fa-bars"></i></a></li>
     </ul>
+    <ul class="nav navbar-nav navbar-right visible-md visible-lg">
+        <li class="dropdown visible-md visible-lg">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="icon-settings"></i></a>
+            <ul class="dropdown-menu">
+                <li class="dropdown-menu-header text-center">
+                    <strong>Account</strong>
+                </li>
+                <li><a href="#"><i class="fa fa-bell-o"></i> Updates <span class="label label-info">42</span></a></li>
+                <li><a href="#"><i class="fa fa-envelope-o"></i> Messages <span class="label label-success">42</span></a></li>
+                <li><a href="#"><i class="fa fa-tasks"></i> Tasks <span class="label label-danger">42</span></a></li>
+                <li><a href="#"><i class="fa fa-comments"></i> Comments <span class="label label-warning">42</span></a></li>
+                <li class="dropdown-menu-header text-center">
+                    <strong>Settings</strong>
+                </li>
+                <li><a href="#"><i class="fa fa-user"></i> Profile</a></li>
+                <li><a href="#"><i class="fa fa-wrench"></i> Settings</a></li>
+                <li><a href="#"><i class="fa fa-usd"></i> Payments <span class="label label-default">42</span></a></li>
+                <li><a href="#"><i class="fa fa-file"></i> Projects <span class="label label-primary">42</span></a></li>
+                <li class="divider"></li>
+                <li><a href="#"><i class="fa fa-shield"></i> Lock Profile</a></li>
+                <li><a href="#"><i class="fa fa-lock"></i> Logout</a></li>
+            </ul>
+        </li>
+    </ul>
 </div>
