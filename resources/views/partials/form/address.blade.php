384c2da12ae83dbdb722a597748e081721127973
Vladilen <vlad@websecret.by>
2015-12-15 16:52:51 +0300

    fix views

--- resources/views/partials/form/address.blade.php
+++ resources/views/partials/form/address.blade.php
@@ -4,7 +4,7 @@
             <div class="form-group {{ PanelForm::formGroupClass('address.'.$addressField, $errors) }}">
                 <label class="col-md-4 control-label">{{ trans('labels.address.'.$addressField) }}</label>
                 <div class="col-md-8">
-                    <input type="text" class="form-control" name="address[{{$addressField}}]" value="{{ Request::old('address.'.$addressField, $address ? $address->{$addressField} : '') }}" >
+                    <input type="text" class="form-control js_panel_input-autocomplete" data-autocomplete-url="{{ $address::getAutocompleteUrl($addressField) }}" name="address[{{$addressField}}]" value="{{ Request::old('address.'.$addressField, $address ? $address->{$addressField} : '') }}" >
                     <div class="error-block">
                         {!! PanelForm::formGroupLabel('address.'.$addressField, $errors) !!}
                     </div>
--- resources/views/partials/form/phones.blade.php
+++ resources/views/partials/form/phones.blade.php
@@ -1,22 +1,20 @@
 <label class="col-md-4 control-label">Phones</label>
 <div class="col-md-8">
     @foreach($phones as $phone)
-        <div class="row mb-15 js_panel_multiple-row" data-name="phone">
-            <div class="col-sm-10 col-md-8 col-lg-9">
-                <input type="text" class="form-control" name="phones[]" value="{{ $phone->phone }}" >
-            </div>
-            <div class="col-sm-2 col-md-4 col-lg-3">
-                <a href="#" class="btn btn-danger btn-block js_panel_multiple-remove" data-name="phone"><span class="fa fa-minus"></span></a>
-            </div>
+        <div class="input-group mb-15 js_panel_multiple-row" data-name="phone">
+            <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+            <input type="text" class="form-control" name="phones[]" value="{{ $phone->phone }}" >
+            <span class="input-group-btn">
+                <a href="#" class="btn btn-danger btn-block js_panel_multiple-remove" data-name="phone"><span class="fa fa-close"></span></a>
+            </span>
         </div>
     @endforeach
-    <div class="row mb-15 js_panel_multiple-row js_panel_multiple-row-clone" data-name="phone">
-        <div class="col-sm-10 col-md-8 col-lg-9">
-            <input type="text" class="form-control" name="phones[]" disabled="disabled" value="" >
-        </div>
-        <div class="col-sm-2 col-md-4 col-lg-3">
-            <a href="#" class="btn btn-danger btn-block js_panel_multiple-remove" data-name="phone"><span class="fa fa-minus"></span></a>
-        </div>
+    <div class="input-group mb-15 js_panel_multiple-row js_panel_multiple-row-clone" data-name="phone">
+        <div class="input-group-addon"><i class="fa fa-phone"></i></div>
+        <input type="text" class="form-control" name="phones[]" disabled="disabled" value="" >
+        <span class="input-group-btn">
+            <a href="#" class="btn btn-danger btn-block js_panel_multiple-remove" data-name="phone"><span class="fa fa-close"></span></a>
+         </span>
     </div>
     <div class="row mb-15">
         <div class="col-sm-12">
