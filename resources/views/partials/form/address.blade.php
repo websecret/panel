bcc4f118d7f12646574c553f8f22f5c30122dd9e
Vladilen <vlad@websecret.by>
2015-12-17 16:37:25 +0300

    fix FormHelper views

--- resources/views/partials/form/address.blade.php
+++ resources/views/partials/form/address.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
     @foreach($fields as $addressField)
         <div class="col-md-12">
-            <div class="form-group {{ PanelForm::formGroupClass('address.'.$addressField, $errors) }}">
+            <div class="form-group {{ FormHelper::formGroupClass('address.'.$addressField, $errors) }}">
                 <label class="col-md-4 control-label">{{ trans('labels.address.'.$addressField) }}</label>
                 <div class="col-md-8">
                     <input type="text" class="form-control js_panel_input-autocomplete" data-autocomplete-url="{{ $address::getAutocompleteUrl($addressField) }}" name="address[{{$addressField}}]" value="{{ Request::old('address.'.$addressField, $address->{$addressField}) }}" >
                     <div class="error-block">
-                        {!! PanelForm::formGroupLabel('address.'.$addressField, $errors) !!}
+                        {!! FormHelper::formGroupLabel('address.'.$addressField, $errors) !!}
                     </div>
                 </div>
             </div>
