11f6aa0a5e384cda19a868acbddb0b293c644244
Vladilen <vlad@websecret.by>
2015-12-17 18:18:06 +0300

    fix FormHelper views

--- resources/views/partials/form/images.blade.php
+++ resources/views/partials/form/images.blade.php
@@ -1,35 +1,47 @@
 <?php $isImageMultiple = $model::getImageIsMultiple($type); ?>
 <div class="js_panel_images-upload-wrapper" data-url="{{ route('panel::upload') }}" data-multiple="{{ $isImageMultiple }}" data-model="{{ get_class($model) }}" data-type="{{ $type }}" data-params="{{ $params }}">
     <div class="js_panel_images-row">
-        <div class="js_panel_images-col js_panel_images-col-clone">
-            <div class="js_panel_images-wrapper">
-                <img class="js_panel_images-img" src="" alt=""/>
-                <a href="#" class="js_panel_images-remove"><span class="fa fa-close"></span></a>
-                <label class="js_panel_images-main">
-                    <input type="radio" name="images[{{ $type }}][main]" value="" disabled="disabled"/>
-                </label>
-                <input class="js_panel_images-path" type="hidden" name="images[{{ $type }}][path][]" value="" disabled="disabled"/>
-            </div>
-        </div>
         <?php $imageKey = 0; ?>
         @foreach($model->images()->ofType($type)->get() as $image)
             <div class="js_panel_images-col">
-                <div class="js_panel_images-wrapper">
+                <input class="js_panel_images-main" type="radio" name="images[{{ $type }}][main]" value="{{ $imageKey }}" {{ $image->main || !$isImageMultiple ? "checked='checked'" : '' }}/>
+                <label class="js_panel_images-wrapper">
                     <img class="js_panel_images-img" src="{{ $model->loadImage($image, $params) }}" alt=""/>
                     <a href="#" class="js_panel_images-remove"><span class="fa fa-close"></span></a>
-                    <label class="js_panel_images-main">
-                        <input type="radio" name="images[{{ $type }}][main]" value="{{ $imageKey }}" {{ $image->main || !$isImageMultiple ? "checked='checked'" : '' }}/>
-                    </label>
                     <input class="js_panel_images-path" type="hidden" name="images[{{ $type }}][path][{{ $imageKey }}]" value="{{ $image->path }}"/>
-                </div>
+                </label>
             </div>
             <?php
-                if(!$isImageMultiple) {
-                    break;
-                }
-                $imageKey++;
+            if(!$isImageMultiple) {
+                break;
+            }
+            $imageKey++;
             ?>
         @endforeach
+        <div class="js_panel_images-col js_panel_images-col-clone">
+            <input class="js_panel_images-main" type="radio" name="images[{{ $type }}][main]" value="" disabled="disabled"/>
+            <label class="js_panel_images-wrapper">
+                <img class="js_panel_images-img" src="" alt=""/>
+                <a href="#" class="js_panel_images-remove"><span class="fa fa-close"></span></a>
+                <input class="js_panel_images-path" type="hidden" name="images[{{ $type }}][path][]" value="" disabled="disabled"/>
+            </label>
+        </div>
+        <div class="js_panel_images-loading-col">
+            <div class="js_panel_images-loading">
+                <div class="js_panel_images-loading1 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading2 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading3 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading4 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading5 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading6 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading7 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading8 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading9 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading10 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading11 js_panel_images-loading-child"></div>
+                <div class="js_panel_images-loading12 js_panel_images-loading-child"></div>
+            </div>
+        </div>
     </div>
     <div>
         <input type="file" {{ $isImageMultiple ? 'multiple="multiple"' : '' }} class="js_panel_images-upload"/>
