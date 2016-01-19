c9fedd4d52636bf811123e41a3fc89c20196e907
Vladilen <vlad@websecret.by>
2016-01-19 13:27:56 +0300

    init 2.0.0

--- src/FormHelperFacade.php
+++ src/FormHelperFacade.php
@@ -6,4 +6,8 @@ use Illuminate\Support\Facades\Facade;
 
 class FormHelperFacade extends Facade
 {
+    protected static function getFacadeAccessor()
+    {
+        return new FormHelperBuilder();
+    }
 }
